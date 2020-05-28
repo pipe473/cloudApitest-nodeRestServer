095d6c777fb4f080f872076878d7aba06394f29a
pipe <felipe.pipe473@gmail.com>
2020-05-28 21:32:24 +0200

    Pasando los datos en formato json

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from my server side');
-})
+  res
+    .status(200)
+    .json({message:'Hello from my custom server side', app:'CloudApi'});
+});
 
 const port = 3000;
 app.listen(port, () => {
