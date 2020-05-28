317108765dd76c3a12c55522a93d23868762139d
pipe <felipe.pipe473@gmail.com>
2020-05-28 21:37:56 +0200

    Creando un metodo post al servidor

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// This endpoint is for request
 app.get('/', (req, res) => {
   res
-    .status(200)
+    .status(404)
     .json({message:'Hello from my custom server side', app:'CloudApi'});
 });
+
+app.post('/', (req, res) => {
+  res.send('You can post something to this endpoint...');
+})
 
 const port = 3000;
 app.listen(port, () => {
