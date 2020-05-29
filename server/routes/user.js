3c37e06d127d782785e63e11d4b797a55d5c6fe8
pipe <felipe.pipe473@gmail.com>
2020-05-29 12:22:52 +0200

    Refactorizando las rutas de los usuarios

---
+++ server/routes/user.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/user', (req, res) => {
+    res.json({message: 'Get User from LOCAL_ENVIRONMENT'});
+});
+
+app.post('/user', (req, res) => {
+
+    let body = req.body;
+
+    if ( body.nombre === undefined ) {
+        
+        res.status(400).json({
+            ok: false,
+            message: 'El nombre es necesario'
+        })
+
+    } else {
+
+        res.json({
+            persona: body
+        });
+    }
+
+});
+
+app.put('/user/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/user', (req, res) => {
+    res.json({message: 'Delete User'});
+});
--- server/server.js
+++ server/server.js
@@ -14,42 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/user', (req, res) => {
-    res.json({message: 'Get User from LOCAL_ENVIRONMENT'});
-});
-
-app.post('/user', (req, res) => {
-
-    let body = req.body;
-
-    if ( body.nombre === undefined ) {
-        
-        res.status(400).json({
-            ok: false,
-            message: 'El nombre es necesario'
-        })
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/user/:id', (req, res) => {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/user', (req, res) => {
-    res.json({message: 'Delete User'});
-});
 
 
 mongoose.connect('mongodb://localhost:27017/cloudapi', (err, res) => {
