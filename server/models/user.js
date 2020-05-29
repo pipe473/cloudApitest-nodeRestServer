fb7106c22049eacc0b06e77267bd22b6e849088c
pipe <felipe.pipe473@gmail.com>
2020-05-29 13:11:00 +0200

    Creando el model, para crear el equema de nuestra db

---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model( 'User', userSchema );
