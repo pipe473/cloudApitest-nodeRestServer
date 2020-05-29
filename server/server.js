e71027cd9d1b8eddd1e9eb45d9c3eb3bff4f793b
pipe <felipe.pipe473@gmail.com>
2020-05-29 12:17:02 +0200

    Base de datos funcionando correctamente

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -46,6 +49,15 @@ app.put('/user/:id', (req, res) => {
 
 app.delete('/user', (req, res) => {
     res.json({message: 'Delete User'});
+});
+
+
+mongoose.connect('mongodb://localhost:27017/cloudapi', (err, res) => {
+
+    if (err)  throw err;
+
+    console.log('Base de datos ONLINE');    
+
 });
 
 app.listen(process.env.PORT, () => {
