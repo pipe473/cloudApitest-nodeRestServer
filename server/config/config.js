39f2eb42741dc0f855bf7c3d9742cc38d0e7fe75
pipe <felipe.pipe473@gmail.com>
2020-06-01 11:02:14 +0200

    Ejemplo de datos necesarios para conectar con mongodb atlas

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,8 @@
 // PUERTO
 //=======================
 
+
+//  mongodb+srv://pipe473:GfdRF5drx9mdmuE1@cluster0-rnpty.mongodb.net/Cloudappi 
+//GfdRF5drx9mdmuE1
+
 process.env.PORT = process.env.PORT || 3000;
