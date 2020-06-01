c5dcc608f130fd631b71529f5bd64e0aa931dd34
pipe <felipe.pipe473@gmail.com>
2020-06-01 12:24:24 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,14 +2,13 @@
 // PUERTO
 //=======================
 
-
 process.env.PORT = process.env.PORT || 3000;
 
 // ======================
 // ENTORNOS
 //=======================
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // ======================
 // BASE DE DATOS
@@ -17,11 +16,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if ( process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cloudapi';
-// } else {
-    urlDB = 'mongodb+srv://pipe473:GfdRF5drx9mdmuE1@cluster0-rnpty.mongodb.net/Cloudappi';
-// }
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cloudapi";
+} else {
+  urlDB =
+    "mongodb+srv://pipe473:GfdRF5drx9mdmuE1@cluster0-rnpty.mongodb.net/Cloudappi";
+}
 process.env.URLDB = urlDB;
- 
+
 // GfdRF5drx9mdmuE1
