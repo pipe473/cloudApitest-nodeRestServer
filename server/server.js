6b1f8b9b1a2f69bd196838e0b9b1938bac5f0be7
pipe <felipe.pipe473@gmail.com>
2020-06-01 11:28:14 +0200

    Conectando base de datos en la nube a entornos de desarrollo y produccion

--- server/server.js
+++ server/server.js
@@ -1,34 +1,31 @@
-require('./config/config');
+require("./config/config");
 
-const express = require('express');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use( require('./routes/user'));
+app.use(require("./routes/user"));
 
-app.use( require('./routes/address'));
+app.use(require("./routes/address"));
 
+mongoose.connect(
+  process.env.URLDB,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  (err, res) => {
+    if (err) throw err;
 
-
-mongoose.connect('mongodb://localhost:27017/cloudapi',
-            { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-            (err, res) => {
-
-    if (err)  throw err;
-
-    console.log('Base de datos ONLINE');    
-
-});
+    console.log("Base de datos ONLINE");
+  }
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
